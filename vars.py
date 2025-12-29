d323afbfb9d0c15a6b71dac96108a3844997bb86
hirensinh90 <hirensinh90@gmail.com>
2025-12-29 11:18:59 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", "294351"))
-API_HASH = os.environ.get("API_HASH", "2d211eb63606daee")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "27788485"))
+API_HASH = os.environ.get("API_HASH", "06c4eac0cbe601f1cf757a2bf579ee54")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8518918286:AAFgZsD_z8emkVN1eDZ-D5Lp93XZzyxCURs")
 
 CREDIT = os.environ.get("CREDIT", "⌯ FʀᴏɴᴛMᴀɴ | ×͜× |")
 # MongoDB Configuration
@@ -13,7 +13,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Dev-Thanos")  # Add
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "7793257011"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8224758311"))
 ADMINS = [int(x) for x in os.environ.get("ADMINS", "7793257011").split()]  # Default to owner ID
 
 # Channel Configuration
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
